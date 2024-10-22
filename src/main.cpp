ab651e9cb920fb2fbbd1c1ef9a297a1d189b3f4e
Alexander Schlake <schlake@3fours.dev>
2024-10-22 18:54:54 -0500

    Working struct serialization

--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,113 @@
 const int UART_RX_PIN = 20;
 const int UART_TX_PIN = 21;
 
+typedef struct __attribute__((packed)) {
+  float kp;
+  float ki;
+  float kd;
+  float bearingPositionToDestinationWaypoint;
+  float destinationLatitude;
+  float destinationLongitude;
+  float previousDestinationLatitude;
+  float previousDestinationLongitude;
+  float heading;
+  float xte;
+  float previousXte;
+  float previousTime;
+  float previousBearing;
+  float integralXTE;
+  float derivativeXTE;
+  float timeDelta;
+  float rudderAngle;
+  float rudderPosition;
+  float targetMotorPosition;
+  bool homingComplete;
+} Autopilot;
+
 BLECharacteristic *pCharacteristic;
 
-String inputString = "";
-bool stringComplete = false;
+byte incomingBuffer[sizeof(Autopilot)];
 
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
-  inputString.reserve(200);
-  BLEDevice::init("BLE Module");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                                          CHARACTERISTIC_UUID,
-                                          BLECharacteristic::PROPERTY_READ |
-                                          BLECharacteristic::PROPERTY_WRITE |
-                                          BLECharacteristic::PROPERTY_NOTIFY
-                                        );
-  pService->addCharacteristic(pCharacteristic);
-  pCharacteristic->setValue("");
-  pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->start();
+  // BLEDevice::init("BLE Module");
+  // BLEServer *pServer = BLEDevice::createServer();
+  // BLEService *pService = pServer->createService(SERVICE_UUID);
+  // pCharacteristic = pService->createCharacteristic(
+  //                                         CHARACTERISTIC_UUID,
+  //                                         BLECharacteristic::PROPERTY_READ |
+  //                                         BLECharacteristic::PROPERTY_WRITE |
+  //                                         BLECharacteristic::PROPERTY_NOTIFY
+  //                                       );
+  // pService->addCharacteristic(pCharacteristic);
+  // pCharacteristic->setValue("");
+  // pService->start();
+  // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  // pAdvertising->start();
 }
 
 void loop() {
-  if (Serial1.available()) {
-    char inChar = (char)Serial1.read();
-    inputString += inChar; 
-    
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
+  static byte buffer[sizeof(Autopilot)];
+  static int bufferIndex = 0;
+  bool dataStarted = false;
+  while (Serial1.available() > 0) {
+    byte incomingByte = Serial1.read();
+    if (incomingByte == 0x02) {  // Start marker detected
+      bufferIndex = 0;
+      dataStarted = true;
+    } else if (dataStarted && bufferIndex < sizeof(Autopilot)) {
+      buffer[bufferIndex++] = incomingByte;
+    } else if (incomingByte == 0x03 && bufferIndex == sizeof(Autopilot)) {  // End marker detected
+      // Deserialize the buffer into the Autopilot struct
+      Autopilot* receivedData = (Autopilot*)buffer;
 
-    if (stringComplete) {
-      Serial.println("Received from MCU: " + inputString);
-      pCharacteristic->setValue(inputString.c_str());
-      pCharacteristic->notify();
-      inputString = "";
-      stringComplete = false;
+      Serial.print("kp: ");
+      Serial.println(receivedData->kp);
+      Serial.print("ki: ");
+      Serial.println(receivedData->ki);
+      Serial.print("kd: ");
+      Serial.println(receivedData->kd);
+      Serial.print("destinationLatitude: ");
+      Serial.println(receivedData->destinationLatitude);
+      Serial.print("destinationLongitude: ");
+      Serial.println(receivedData->destinationLongitude);
+      Serial.print("bearingPositionToDestinationWaypoint: ");
+      Serial.println(receivedData->bearingPositionToDestinationWaypoint);
+      Serial.print("heading: ");
+      Serial.println(receivedData->heading);
+      Serial.print("xte: ");
+      Serial.println(receivedData->xte);
+      Serial.print("previousXte: ");
+      Serial.println(receivedData->previousXte);
+      Serial.print("previousTime: ");
+      Serial.println(receivedData->previousTime);
+      Serial.print("previousBearing: ");
+      Serial.println(receivedData->previousBearing);
+      Serial.print("integralXTE: ");
+      Serial.println(receivedData->integralXTE);
+      Serial.print("derivativeXTE: ");
+      Serial.println(receivedData->derivativeXTE);
+      Serial.print("timeDelta: ");
+      Serial.println(receivedData->timeDelta);
+      Serial.print("rudderAngle: ");
+      Serial.println(receivedData->rudderAngle);
+      Serial.print("rudderPosition: ");
+      Serial.println(receivedData->rudderPosition);
+      Serial.print("targetMotorPosition: ");
+      Serial.println(receivedData->targetMotorPosition);
+      Serial.println("==================================");
+
+      bufferIndex = 0;
+      dataStarted = false;
     }
   }
 
   // if (pCharacteristic->getValue().length() > 0) {
-  //   String bleData = pCharacteristic->getValue().c_str();
-  //   Serial.println("Received from BLE: " + bleData);
-  //   Serial1.print(bleData);
-  //   pCharacteristic->setValue("");
+    // String bleData = pCharacteristic->getValue().c_str();
+    // Serial.println("Received from BLE: " + bleData);
+    // Serial1.print(bleData);
+    // pCharacteristic->setValue("");
   // }
 
   delay(100);
