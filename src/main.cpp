07f6a5e5a06f673493224adcec8d1a69b129f716
Alexander Schlake <schlake@3fours.dev>
2024-10-24 00:09:00 -0500

    Send BLE data to teensy on write to characteristic

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,11 @@ typedef struct __attribute__((packed)) {
 BLECharacteristic *pCharacteristic;
 BLEAdvertising* pAdvertising = NULL;
 byte incomingBuffer[sizeof(Autopilot)];
+std::string bleData;
 bool deviceConnected = false;
+bool isWritten = false;
+byte startMarker = 0x02;
+byte endMarker = 0x03;
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -46,10 +50,15 @@ class MyServerCallbacks : public BLEServerCallbacks {
   void onDisconnect(BLEServer* pServer) {
     Serial.println("Device disconnected");
     deviceConnected = false;
-    
-    // Restart advertising so the ESP32 can be discovered again
     pAdvertising->start();
     Serial.println("Advertising restarted");
+  }
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    bleData = pCharacteristic->getValue();
+    isWritten = true;
   }
 };
 
@@ -67,6 +76,7 @@ void setup() {
                                           BLECharacteristic::PROPERTY_WRITE |
                                           BLECharacteristic::PROPERTY_NOTIFY
                                         );
+  pCharacteristic->setCallbacks(new MyCallbacks());
   pService->addCharacteristic(pCharacteristic);
   pCharacteristic->setValue("");
   pService->start();
@@ -79,69 +89,72 @@ void loop() {
   static byte buffer[sizeof(Autopilot)];
   static int bufferIndex = 0;
   bool dataStarted = false;
+
   while (Serial1.available()) {
     byte incomingByte = Serial1.read();
-    if (incomingByte == 0x02) {  // Start marker detected
+    if (incomingByte == startMarker) {  // Start marker detected
       bufferIndex = 0;
       dataStarted = true;
     } else if (dataStarted && bufferIndex < sizeof(Autopilot)) {
       buffer[bufferIndex++] = incomingByte;
-    } else if (incomingByte == 0x03 && bufferIndex == sizeof(Autopilot)) {  // End marker detected
+    } else if (incomingByte == endMarker && bufferIndex == sizeof(Autopilot)) {  // End marker detected
       // Deserialize the buffer into the Autopilot struct
       Autopilot* receivedData = (Autopilot*)buffer;
 
       if (deviceConnected) {
-        pCharacteristic->setValue(buffer, sizeof(Autopilot));
-        pCharacteristic->notify();
+        if (isWritten) {
+          Serial1.write(startMarker);
+          for (int i = 0; i < bleData.length(); i++) {
+            Serial1.write(bleData[i]);
+          }
+          Serial1.write(endMarker);
+          isWritten = false;
+        } else {
+          pCharacteristic->setValue(buffer, sizeof(Autopilot));
+          pCharacteristic->notify();
+        }
       }
 
-      Serial.print("kp: ");
-      Serial.println(receivedData->kp);
-      Serial.print("ki: ");
-      Serial.println(receivedData->ki);
-      Serial.print("kd: ");
-      Serial.println(receivedData->kd);
-      Serial.print("destinationLatitude: ");
-      Serial.println(receivedData->destinationLatitude);
-      Serial.print("destinationLongitude: ");
-      Serial.println(receivedData->destinationLongitude);
-      Serial.print("bearingPositionToDestinationWaypoint: ");
-      Serial.println(receivedData->bearingPositionToDestinationWaypoint);
-      Serial.print("heading: ");
-      Serial.println(receivedData->heading);
-      Serial.print("xte: ");
-      Serial.println(receivedData->xte);
-      Serial.print("previousXte: ");
-      Serial.println(receivedData->previousXte);
-      Serial.print("previousTime: ");
-      Serial.println(receivedData->previousTime);
-      Serial.print("previousBearing: ");
-      Serial.println(receivedData->previousBearing);
-      Serial.print("integralXTE: ");
-      Serial.println(receivedData->integralXTE);
-      Serial.print("derivativeXTE: ");
-      Serial.println(receivedData->derivativeXTE);
-      Serial.print("timeDelta: ");
-      Serial.println(receivedData->timeDelta);
-      Serial.print("rudderAngle: ");
-      Serial.println(receivedData->rudderAngle);
-      Serial.print("rudderPosition: ");
-      Serial.println(receivedData->rudderPosition);
-      Serial.print("targetMotorPosition: ");
-      Serial.println(receivedData->targetMotorPosition);
-      Serial.println("==================================");
+      // Serial.print("kp: ");
+      // Serial.println(receivedData->kp);
+      // Serial.print("ki: ");
+      // Serial.println(receivedData->ki);
+      // Serial.print("kd: ");
+      // Serial.println(receivedData->kd);
+      // Serial.print("destinationLatitude: ");
+      // Serial.println(receivedData->destinationLatitude);
+      // Serial.print("destinationLongitude: ");
+      // Serial.println(receivedData->destinationLongitude);
+      // Serial.print("bearingPositionToDestinationWaypoint: ");
+      // Serial.println(receivedData->bearingPositionToDestinationWaypoint);
+      // Serial.print("heading: ");
+      // Serial.println(receivedData->heading);
+      // Serial.print("xte: ");
+      // Serial.println(receivedData->xte);
+      // Serial.print("previousXte: ");
+      // Serial.println(receivedData->previousXte);
+      // Serial.print("previousTime: ");
+      // Serial.println(receivedData->previousTime);
+      // Serial.print("previousBearing: ");
+      // Serial.println(receivedData->previousBearing);
+      // Serial.print("integralXTE: ");
+      // Serial.println(receivedData->integralXTE);
+      // Serial.print("derivativeXTE: ");
+      // Serial.println(receivedData->derivativeXTE);
+      // Serial.print("timeDelta: ");
+      // Serial.println(receivedData->timeDelta);
+      // Serial.print("rudderAngle: ");
+      // Serial.println(receivedData->rudderAngle);
+      // Serial.print("rudderPosition: ");
+      // Serial.println(receivedData->rudderPosition);
+      // Serial.print("targetMotorPosition: ");
+      // Serial.println(receivedData->targetMotorPosition);
+      // Serial.println("==================================");
 
       bufferIndex = 0;
       dataStarted = false;
     }
   }
-
-  // if (pCharacteristic->getValue().length() > 0) {
-    // String bleData = pCharacteristic->getValue().c_str();
-    // Serial.println("Received from BLE: " + bleData);
-    // Serial1.print(bleData);
-    // pCharacteristic->setValue("");
-  // }
 
   delay(500);
 }
